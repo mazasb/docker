3d7cef251ff7f910f0576edf6555d6638fcea902
Mázás Balázs <bmazas@loginet.hu>
2017-11-13 11:09:30 +0100

    Fix ssh config

--- php7.1-builder/entrypoint.sh
+++ php7.1-builder/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-eval $(ssh-agent -s)
-mkdir -p ~/.ssh
-if [[ -z "${SSH_PRIVATE_KEY}" ]]; then
+set -e
+
+if [ "$SSH_PRIVATE_KEY" != "" ]; then
+    eval $(ssh-agent -s)
+    mkdir -p ~/.ssh
     echo "$SSH_PRIVATE_KEY" | ssh-add -
+    [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 fi
-[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
 exec "$@"
