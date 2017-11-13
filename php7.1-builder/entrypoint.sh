c8a61bd9aa5f202386c5f9a46309370bb06902be
Mázás Balázs <bmazas@loginet.hu>
2017-11-13 13:37:09 +0100

    Fix docker socket binding Add docker-compose

--- php7.1-builder/entrypoint.sh
+++ php7.1-builder/entrypoint.sh
@@ -9,4 +9,8 @@ if [ "$SSH_PRIVATE_KEY" != "" ]; then
     [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 fi
 
+if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
+	export DOCKER_HOST='tcp://docker:2375'
+fi
+
 exec "$@"
